cf50a5fce1429a456bde6984d2947a542921761e
andycall <andycall@openwebf.com>
2025-06-28 10:17:17 +0800

    feat: add context menu and tab bar types

---
+++ lib/src/context-menu.d.ts
@@ -0,0 +1,20 @@
+interface ContextMenuAction {
+  text: string;
+  icon?: string;
+  destructive?: boolean;
+  default?: boolean;
+  event?: string;
+}
+
+interface FlutterCupertinoContextMenuProperties {
+  'enable-haptic-feedback'?: boolean;
+  
+  // Methods
+  setActions(actions: ContextMenuAction[]): void;
+}
+
+interface FlutterCupertinoContextMenuEvents {
+  // Dynamic events based on action configuration
+  // Default event name is 'press' if not specified in action
+  [eventName: string]: Event;
+}
--- lib/src/tab_bar.dart
+++ lib/src/tab_bar.dart
@@ -3,6 +3,7 @@
  * Licensed under GNU AGPL with Enterprise exception.
  */
 import 'package:flutter/cupertino.dart';
+import 'package:webf/rendering.dart';
 import 'package:webf/webf.dart';
 import 'package:webf/dom.dart' as dom;
 
@@ -36,13 +37,117 @@ class FlutterTabBar extends WidgetElement {
       ];
 
   static final Map<String, IconData> _iconMap = {
+    // Common navigation icons
     'home': CupertinoIcons.home,
+    'house': CupertinoIcons.house,
+    'house_fill': CupertinoIcons.house_fill,
     'search': CupertinoIcons.search,
+    'search_circle': CupertinoIcons.search_circle,
+    'search_circle_fill': CupertinoIcons.search_circle_fill,
+    
+    // Action icons
     'add': CupertinoIcons.add,
-    'bell': CupertinoIcons.bell,
-    'person': CupertinoIcons.person,
+    'add_circled': CupertinoIcons.add_circled,
     'add_circled_solid': CupertinoIcons.add_circled_solid,
-    // ...
+    'plus': CupertinoIcons.plus,
+    'plus_circle': CupertinoIcons.plus_circle,
+    'plus_circle_fill': CupertinoIcons.plus_circle_fill,
+    
+    // User/Account icons
+    'person': CupertinoIcons.person,
+    'person_fill': CupertinoIcons.person_fill,
+    'person_circle': CupertinoIcons.person_circle,
+    'person_circle_fill': CupertinoIcons.person_circle_fill,
+    'profile_circled': CupertinoIcons.profile_circled,
+    
+    // Communication icons
+    'bell': CupertinoIcons.bell,
+    'bell_fill': CupertinoIcons.bell_fill,
+    'bell_circle': CupertinoIcons.bell_circle,
+    'bell_circle_fill': CupertinoIcons.bell_circle_fill,
+    'chat_bubble': CupertinoIcons.chat_bubble,
+    'chat_bubble_fill': CupertinoIcons.chat_bubble_fill,
+    'chat_bubble_2': CupertinoIcons.chat_bubble_2,
+    'chat_bubble_2_fill': CupertinoIcons.chat_bubble_2_fill,
+    'mail': CupertinoIcons.mail,
+    'mail_solid': CupertinoIcons.mail_solid,
+    'envelope': CupertinoIcons.envelope,
+    'envelope_fill': CupertinoIcons.envelope_fill,
+    'phone': CupertinoIcons.phone,
+    'phone_fill': CupertinoIcons.phone_fill,
+    
+    // Navigation/Direction icons
+    'compass': CupertinoIcons.compass,
+    'compass_fill': CupertinoIcons.compass_fill,
+    'location': CupertinoIcons.location,
+    'location_fill': CupertinoIcons.location_fill,
+    'map': CupertinoIcons.map,
+    'map_fill': CupertinoIcons.map_fill,
+    
+    // Media icons
+    'photo': CupertinoIcons.photo,
+    'photo_fill': CupertinoIcons.photo_fill,
+    'camera': CupertinoIcons.camera,
+    'camera_fill': CupertinoIcons.camera_fill,
+    'video_camera': CupertinoIcons.video_camera,
+    'video_camera_solid': CupertinoIcons.video_camera_solid,
+    'play': CupertinoIcons.play,
+    'play_fill': CupertinoIcons.play_fill,
+    'play_circle': CupertinoIcons.play_circle,
+    'play_circle_fill': CupertinoIcons.play_circle_fill,
+    
+    // Settings/System icons
+    'gear': CupertinoIcons.gear,
+    'gear_solid': CupertinoIcons.gear_solid,
+    'settings': CupertinoIcons.settings,
+    'settings_solid': CupertinoIcons.settings_solid,
+    'ellipsis': CupertinoIcons.ellipsis,
+    'ellipsis_circle': CupertinoIcons.ellipsis_circle,
+    'ellipsis_circle_fill': CupertinoIcons.ellipsis_circle_fill,
+    
+    // Business/Finance icons
+    'creditcard': CupertinoIcons.creditcard,
+    'creditcard_fill': CupertinoIcons.creditcard_fill,
+    'cart': CupertinoIcons.cart,
+    'cart_fill': CupertinoIcons.cart_fill,
+    'bag': CupertinoIcons.bag,
+    'bag_fill': CupertinoIcons.bag_fill,
+    
+    // Document/File icons
+    'doc': CupertinoIcons.doc,
+    'doc_fill': CupertinoIcons.doc_fill,
+    'doc_text': CupertinoIcons.doc_text,
+    'doc_text_fill': CupertinoIcons.doc_text_fill,
+    'folder': CupertinoIcons.folder,
+    'folder_fill': CupertinoIcons.folder_fill,
+    'book': CupertinoIcons.book,
+    'book_fill': CupertinoIcons.book_fill,
+    
+    // Social/Interaction icons
+    'heart': CupertinoIcons.heart,
+    'heart_fill': CupertinoIcons.heart_fill,
+    'star': CupertinoIcons.star,
+    'star_fill': CupertinoIcons.star_fill,
+    'hand_thumbsup': CupertinoIcons.hand_thumbsup,
+    'hand_thumbsup_fill': CupertinoIcons.hand_thumbsup_fill,
+    'bookmark': CupertinoIcons.bookmark,
+    'bookmark_fill': CupertinoIcons.bookmark_fill,
+    
+    // Currency icons (using money_dollar as bitcoin alternative)
+    'money_dollar': CupertinoIcons.money_dollar,
+    'money_dollar_circle': CupertinoIcons.money_dollar_circle,
+    'money_dollar_circle_fill': CupertinoIcons.money_dollar_circle_fill,
+    
+    // Utility icons
+    'info': CupertinoIcons.info,
+    'info_circle': CupertinoIcons.info_circle,
+    'info_circle_fill': CupertinoIcons.info_circle_fill,
+    'question': CupertinoIcons.question,
+    'question_circle': CupertinoIcons.question_circle,
+    'question_circle_fill': CupertinoIcons.question_circle_fill,
+    'exclamationmark': CupertinoIcons.exclamationmark,
+    'exclamationmark_circle': CupertinoIcons.exclamationmark_circle,
+    'exclamationmark_circle_fill': CupertinoIcons.exclamationmark_circle_fill,
   };
 
   List<BottomNavigationBarItem> _buildTabItems() {
@@ -150,7 +255,8 @@ class FlutterTabBarState extends WebFWidgetElementState {
                 border: isDark
                     ? Border(
                         top: BorderSide(
-                            color: CupertinoDynamicColor.resolve(CupertinoColors.separator, context).withValues(alpha: 0.3)))
+                            color: CupertinoDynamicColor.resolve(CupertinoColors.separator, context)
+                                .withValues(alpha: 0.3)))
                     : null,
                 onTap: (index) {
                   widgetElement.setAttribute('currentIndex', index.toString());
@@ -160,7 +266,7 @@ class FlutterTabBarState extends WebFWidgetElementState {
               return CupertinoTabView(
                 builder: (BuildContext context) {
                   if (index < tabViews.length) {
-                    return tabViews[index];
+                    return WebFWidgetElementChild(child: tabViews[index]);
                   }
                   return Center(
                     child: Text(
@@ -196,10 +302,11 @@ class FlutterCupertinoTabBarItemState extends WebFWidgetElementState {
 
   @override
   Widget build(BuildContext context) {
-    return WebFHTMLElement(
-        tagName: 'DIV',
-        controller: widgetElement.ownerDocument.controller,
-        parentElement: widgetElement,
-        children: widgetElement.childNodes.toWidgetList());
+    return WebFWidgetElementChild(
+        child: WebFHTMLElement(
+            tagName: 'DIV',
+            controller: widgetElement.ownerDocument.controller,
+            parentElement: widgetElement,
+            children: widgetElement.childNodes.toWidgetList()));
   }
 }
